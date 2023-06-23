24bc88ae6d9539bf1659756c979cd7dc8e5f7652
NehalBothra <46522615+NehalBothra@users.noreply.github.com>
2023-06-23 21:56:57 +0530

    Add files via upload

---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">Amazon</a>
+                        </div>
+                        <ul className="nav navbar-nav">
+                            <li className="active"><a href="#">Home</a></li>
+                            <li><a href="#">Page 1</a></li>
+                            <li><a href="#">Page 2</a></li>
+                            <li><a href="#">Page 3</a></li>
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+                    </div>
+                </nav>
+
+            </>
+        )
+    }
+}
+export default NavBar
