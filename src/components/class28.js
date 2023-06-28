7c2e53d21ca2b6e0611b5170e37a50d7a0e0bf1a
NehalBothra <46522615+NehalBothra@users.noreply.github.com>
2023-06-28 21:02:50 +0530

    class28.js

--- src/components/class28.js
+++ src/components/class28.js
@@ -8,12 +8,16 @@ class States extends React.Component{
             year:1993
         }
     }
+    changingText=()=>{
+        this.setState({coding:"CSS",founder:"HÃ¥kon Wium Lie", year:1994})
+    }
     render(){
         return(
             <>
             <h1>We are using {this.state.coding}</h1>
             <h2>The founder of {this.state.coding} is {this.state.founder}</h2>
             <p>It was found in {this.state.year}</p>
+            <button onClick={this.changingText}>Changing</button>
             </>
         )
     }
