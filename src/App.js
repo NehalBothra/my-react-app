fab33430b5a3eda25337cfc6988a6e2107f16eb9
NehalBothra <46522615+NehalBothra@users.noreply.github.com>
2023-07-01 22:01:26 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import NavBar from '../src/components/navbar'
 import States from './components/class28';
 import Garage from './components/class29';
+import Computer from './components/class 27';
 function App() {
   return (
     <>
      <NavBar></NavBar>
      <States></States>
      <Garage></Garage>
+     <Computer></Computer>
     </>
 
   );
